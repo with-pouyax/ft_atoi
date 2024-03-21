2b1af9d6bc383a8dc9854bb8fb9e0af6c5e9e226
Pouya Ghajardavalou <ghajar.pouya@gmail.com>
2024-03-21 10:39:22 -0700

    Add files via upload

---
+++ ft_atoi.c
@@ -0,0 +1,22 @@
+int	ft_atoi(char *str)
+{
+	int	r;
+	int	s;
+
+	r = 0;
+	s = 1;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	while (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			s *= -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		r = r * 10 + *str - '0';
+		str++;
+	}
+	return (r * s);
+}
